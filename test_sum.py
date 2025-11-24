7e120b949d8565223f4f16e533a97ccc0bab6282
Spoorthi s patil <spoorthipatil219@gmail.com>
2025-11-24 16:37:28 +0530

    Update test_sum.py

--- test_sum.py
+++ test_sum.py
@@ -1,7 +1,7 @@
 from sum import sum_numbers
 def test_add_positive_numbers():
-    assert add(2, 3) == 5
+    assert sum_numbers(2, 3) == 5
 def test_add_negative_numbers():
-    assert add(-4,-6) == -10
+    assert sum_numbers(-4,-6) == -10
 def test_add_zero():
-    assert add(0, 5) == 5
+    assert sum_numbers(0, 5) == 5
