661b0d6c80a61c893ff7519f624aca964c4478e3
Spoorthi s patil <spoorthipatil219@gmail.com>
2025-11-24 16:25:38 +0530

    Update sum.py

--- sum.py
+++ sum.py
@@ -1,13 +1,15 @@
 import sys
 def sum_numbers(x, y):
     return x + y
-if len(sys.argv) == 3:
-    script_name = sys.argv[0]
-    a = int(sys.argv[1])
-    b = int(sys.argv[2])
-else:
-    script_name = sys.argv[0]
-    a = 20
-    b = 30
-result = sum_numbers(a, b)
-print("Sum:", result)
+if __name__=="__main__":
+    
+    if len(sys.argv) == 3:
+        script_name = sys.argv[0]
+        a = int(sys.argv[1])
+        b = int(sys.argv[2])
+    else:
+        script_name = sys.argv[0]
+        a = 20
+        b = 30
+    result = sum_numbers(a, b)
+    print("Sum:", result)
