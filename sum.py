00f9e29f1bd43245ca72964829a64e5067534dc5
Spoorthi s patil <spoorthipatil219@gmail.com>
2025-11-24 16:22:16 +0530

    Update sum.py

--- sum.py
+++ sum.py
@@ -1,11 +1,13 @@
 import sys
-if len(sys.argv)==3:
-  script_name=sys.argv[0]
-  a=sys.argv[1]
-  b=sys.argv[2]
+def sum_numbers(x, y):
+    return x + y
+if len(sys.argv) == 3:
+    script_name = sys.argv[0]
+    a = int(sys.argv[1])
+    b = int(sys.argv[2])
 else:
-  script_name=sys.argv[0]
-  a=20
-  b=30
-sum=a+b
-print("Sum:",sum)
+    script_name = sys.argv[0]
+    a = 20
+    b = 30
+result = sum_numbers(a, b)
+print("Sum:", result)
